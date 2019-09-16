f1de70ae2a8e29f9d978b803a2cf9cdd156e1270
kirill <kirill@Mac-mini-kirill.local>
2019-09-16 14:50:37 +0300

    cors-test

--- middleware/cors.js
+++ middleware/cors.js
@@ -1,8 +1,9 @@
 module.exports = function(server) {
   server.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 }
