31e6ac229111b976db2802726eb3eb5d4267281a
kirill <kirill@Mac-mini-kirill.local>
2019-09-24 10:00:52 +0300

    add keys.prod value

--- keys/keys.prod.js
+++ keys/keys.prod.js
@@ -1,3 +1,6 @@
 module.exports = {
-  MONGO_URI: process.env.MONGO_URI
+  MONGO_URI: process.env.MONGO_URI,
+  fireBasePrivateKeyPath: '../keys/photostock-storage-firebase-adminsdk-h94zd-ab95f3eb9e.json',
+  firebaseStorageBucketURL: 'gs://photostock-storage.appspot.com',
+  myprivatekey: process.env.myprivatekey
 }
