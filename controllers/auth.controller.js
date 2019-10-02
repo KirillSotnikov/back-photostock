ca944b5918a3d9b7a649dd276802878480e90700
kirill <kirill@Mac-mini-kirill.local>
2019-10-02 13:17:29 +0300

    test-cors - 2

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -27,24 +27,28 @@ module.exports.createUser = async (req, res) => {
 }
 
 module.exports.login = async (req, res) => {
-  let user = await User.findOne({email: req.body.email})
-
-  if (user) {
-    const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password)
-
-    if(isPasswordCorrect) {
-      const token = user.generateAuthToken()
-
-      res.json({
-        status:'success',
-        data: { token, user }
-      })
+  try{
+    let user = await User.findOne({email: req.body.email})
+  
+    if (user) {
+      const isPasswordCorrect = bcrypt.compareSync(req.body.password, user.password)
+  
+      if(isPasswordCorrect) {
+        const token = user.generateAuthToken()
+  
+        res.json({
+          status:'success',
+          data: { token, user }
+        })
+      } else {
+        throw new WrongData()
+      }
     } else {
-      throw new WrongData()
+      throw new NotFoundError()
+      // res.send(new NotFoundError())
     }
-  } else {
-    throw new NotFoundError()
-    // res.send(new NotFoundError())
+  } catch (err) {
+    res.error(err)
   }
 }
 
